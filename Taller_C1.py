4ee7663694e04f42202faf895ab162c5e453929d
Desarrollo <Desarrollo@DESKTOP-F722E5D>
2021-09-04 20:37:47 -0500

    ejercicio 2 parte 1

--- Taller_C1.py
+++ Taller_C1.py
@@ -24,6 +24,19 @@ ej1_1resultado = ((((5 + 2 - 5) ** 2) * 5 + (8 / 2) - 30) / 2) * 5 - 3
 print(f"ej 1.1 y = ((((5+2-5)**2) * 5 + (8/2) -30 ) / 2 )* 5 -3")
 print(f"ej 1.1 Resultado: {ej1_1resultado}")
 
+# ------Calcule el valor de Y indicando paso a paso como llegó al resultado 1.2
+
+# z=5
+# n=3
+# m= z-n
+# y = (( (z+2-n)^2 * m+8/2 -30 ) / 2 * 5 -3)^ 5 + 15 * 3 - 9/3 
+
+ej1_2z = 5
+ej1_2n = 3
+ej1_2m = ej1_2z - ej1_2n
+ej1_2resultado = float((((((ej1_2z + 2 - ej1_2n)** 2) * ej1_2m + 8 / 2 - 30) / 2) * 5 - 3)** 5 + 15 * 3 - (9 / 3))
+print("ej 1.2 y = ((((5+2-5)**2) * 5 + (8/2) -30 ) / 2 )* 5 -3")
+print(f"ej 1.2 Resultado: {ej1_2resultado}")
 
 
 
