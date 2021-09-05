68f13ea54b1bd1d02c8e5638c5c1c07d47d83be8
Desarrollo <Desarrollo@DESKTOP-F722E5D>
2021-09-04 20:34:46 -0500

    ejercicio 1 parte 1

--- Taller_C1.py
+++ Taller_C1.py
@@ -10,3 +10,38 @@ Created on Sat Sep  4 17:55:21 2021
 
 
 # prueba conexion
+
+# ------Calcule el valor de Y indicando paso a paso como llegó al resultado 1.1
+# 1.1    y = ( (5+2-5)^2 * 5+8/2 -30 ) / 2 * 5 -3
+# 1.1    y = ( (2)^2 * 5+ 4 -30 ) / 2 * 5 -3
+# 1.1    y = ( 4 * 5 + 4 - 30 ) / 2 * 5 -3
+# 1.1    y = (20 + 4 -30 ) / 2 * 5 -3
+# 1.1    y = (-6) / 2 * 5 -3
+# 1.1    y = -3 * 5 -3
+# 1.1    y = -18
+
+ej1_1resultado = ((((5 + 2 - 5) ** 2) * 5 + (8 / 2) - 30) / 2) * 5 - 3
+print(f"ej 1.1 y = ((((5+2-5)**2) * 5 + (8/2) -30 ) / 2 )* 5 -3")
+print(f"ej 1.1 Resultado: {ej1_1resultado}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
